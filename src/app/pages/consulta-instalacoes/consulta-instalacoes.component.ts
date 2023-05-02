807c41fd85f901f4b259e1371de594b2ccbe5a5e
bruno.pereira <bruno.pereira@amcom.com.br>
2023-05-02 09:19:59 -0300

    #92207 - Ordenacao Colunas

--- src/app/pages/consulta-instalacoes/consulta-instalacoes.component.ts
+++ src/app/pages/consulta-instalacoes/consulta-instalacoes.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {ConsultaInstalacoesService} from "./services/consulta-instalacoes.service";
 import {MatTableDataSource} from "@angular/material/table";
-import {MatSort} from "@angular/material/sort";
+import {MatSort, Sort} from "@angular/material/sort";
 import {LiveAnnouncer} from "@angular/cdk/a11y";
 
 @Component({
@@ -16,7 +16,7 @@ export class ConsultaInstalacoesComponent implements OnInit {
   @ViewChild(MatSort) sort!: MatSort;
 
   displayedColumns: string [] = ['TipoInstalacao.Nome', 'NomeInstalacao', 'Agente',
-    'Area', 'Sigla','PontoConexao','PotenciaInstalada','NomeCondicaoOperacao'];
+    'Area', 'Sigla', 'PontoConexao', 'PotenciaInstalada', 'NomeCondicaoOperacao'];
 
   constructor(
     private _liveAnnouncer: LiveAnnouncer,
@@ -31,9 +31,134 @@ export class ConsultaInstalacoesComponent implements OnInit {
     this.consultaInstalacoesService.getInstalacoes().subscribe((item) => {
       this.dataSource.data = item.content;
       this.dataSource.sort = this.sort;
-
       this.loading = false;
     })
+  }
+
+  onSortChange(event: Sort) {
+    switch (event.active) {
+      case "TipoInstalacao.Nome":
+        return this.ordernaFonte(event);
+      case"NomeInstalacao":
+        return this.ordernaNomeInstalacao(event);
+      case "Agente":
+        return this.ordernaAgente(event);
+      case "Area":
+        return this.ordernaArea(event);
+      case "Sigla":
+        return this.ordernaSigla(event);
+      case "PontoConexao":
+        return this.ordernaPontoConexao(event);
+      case "PotenciaInstalada":
+        return this.ordernaPotenciaInstalada(event);
+      case "NomeCondicaoOperacao":
+        return this.ordernaNomeCondicaoOperacao(event);
+    }
+  }
+
+  private ordernaFonte(event: Sort) {
+    this.dataSource.data = this.dataSource.data.sort((a, b) => {
+      const lastNameA = a.TipoInstalacao.Nome;
+      const lastNameB = b.TipoInstalacao.Nome;
+
+      if (event.direction === 'asc') {
+        return lastNameA < lastNameB ? -1 : 1;
+      } else {
+        return lastNameA > lastNameB ? -1 : 1;
+      }
+    });
+  }
+
+  private ordernaNomeInstalacao(event: Sort) {
+    this.dataSource.data = this.dataSource.data.sort((a, b) => {
+      const lastNameA = a.NomeInstalacao;
+      const lastNameB = b.NomeInstalacao;
+
+      if (event.direction === 'asc') {
+        return lastNameA < lastNameB ? -1 : 1;
+      } else {
+        return lastNameA > lastNameB ? -1 : 1;
+      }
+    });
+  }
+
+  private ordernaAgente(event: Sort) {
+    this.dataSource.data = this.dataSource.data.sort((a, b) => {
+      const lastNameA = a.Agente;
+      const lastNameB = b.Agente;
+
+      if (event.direction === 'asc') {
+        return lastNameA < lastNameB ? -1 : 1;
+      } else {
+        return lastNameA > lastNameB ? -1 : 1;
+      }
+    });
+  }
+
+  private ordernaArea(event: Sort) {
+    this.dataSource.data = this.dataSource.data.sort((a, b) => {
+      const lastNameA = a.Area;
+      const lastNameB = b.Area;
+
+      if (event.direction === 'asc') {
+        return lastNameA < lastNameB ? -1 : 1;
+      } else {
+        return lastNameA > lastNameB ? -1 : 1;
+      }
+    });
+  }
+
+  private ordernaSigla(event: Sort) {
+    this.dataSource.data = this.dataSource.data.sort((a, b) => {
+      const lastNameA = a.SiglaConexao;
+      const lastNameB = b.SiglaConexao;
+
+      if (event.direction === 'asc') {
+        return lastNameA < lastNameB ? -1 : 1;
+      } else {
+        return lastNameA > lastNameB ? -1 : 1;
+      }
+    });
+
+  }
+
+  private ordernaPontoConexao(event: Sort) {
+    this.dataSource.data = this.dataSource.data.sort((a, b) => {
+      const lastNameA = a.PontoConexao;
+      const lastNameB = b.PontoConexao;
+
+      if (event.direction === 'asc') {
+        return lastNameA < lastNameB ? -1 : 1;
+      } else {
+        return lastNameA > lastNameB ? -1 : 1;
+      }
+    });
+  }
+
+  private ordernaPotenciaInstalada(event: Sort) {
+    this.dataSource.data = this.dataSource.data.sort((a, b) => {
+      const lastNameA = a.PotenciaInstalada;
+      const lastNameB = b.PotenciaInstalada;
+
+      if (event.direction === 'asc') {
+        return lastNameA < lastNameB ? -1 : 1;
+      } else {
+        return lastNameA > lastNameB ? -1 : 1;
+      }
+    });
+  }
+
+  private ordernaNomeCondicaoOperacao(event: Sort) {
+    this.dataSource.data = this.dataSource.data.sort((a, b) => {
+      const lastNameA = a.NomeCondicaoOperacao;
+      const lastNameB = b.NomeCondicaoOperacao;
+
+      if (event.direction === 'asc') {
+        return lastNameA < lastNameB ? -1 : 1;
+      } else {
+        return lastNameA > lastNameB ? -1 : 1;
+      }
+    });
   }
 
 }
